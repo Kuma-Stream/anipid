ecdf11347bdd87d84e2c039d9d4357f548aff537
Kuma-Stream <121804937+Kuma-Stream@users.noreply.github.com>
2023-10-01 12:45:05 +0700

    a

--- anibot/__main__.py
+++ anibot/__main__.py
@@ -8,12 +8,19 @@ if has_user:
     from . import user
 
 async def main():
-    await anibot.start()
-    if user is not None:
-        await user.start()
+    async with asyncio.gather(
+        anibot.start(),
+        user.start() if user is not None else asyncio.sleep(0)
+    ) as gather_tasks:
+        await asyncio.wait(gather_tasks)
+
     await idle()
+
     await anibot.stop()
     if user is not None:
         await user.stop()
     _close_db()
     await session.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
